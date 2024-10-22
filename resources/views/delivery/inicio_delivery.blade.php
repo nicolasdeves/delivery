96de6fc0bd8fd853742e2481fb6d42fedcf33b62
Nícolas Deves <nicolas.deves@universo.univates.br>
2024-10-21 21:35:10 -0300

    Feat: adiciona tela do delivery

--- resources/views/delivery/inicio_delivery.blade.php
+++ resources/views/delivery/inicio_delivery.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Cardápio - LaPorto</title>
     <link rel="icon" href="../images/laporto-icon.ico">
 
@@ -31,226 +32,124 @@
                     <a href="#burgs"><button class="active"><i class="fas fa-hamburger"></i>&nbsp;Burgs</button></a>
                     <a href="#burgsBatata"><button><i class="fas fa-hamburger"></i>&nbsp;Burgs com
                             Batata</button> </a>
-                    <a href="#entradas"><button><i
-                                class="fa-solid fa-bacon"></i>&nbsp;Entradas</button></a>
-                    <a href="#rangos"><button><i
-                                class="fa-solid fa-drumstick-bite"></i>&nbsp;Rangos</button></a>
-                    <a href="#drinks"><button><i
-                                class="fa-solid fa-martini-glass-citrus"></i>&nbsp;Drinks</button></a>
+                    <a href="#entradas"><button><i class="fa-solid fa-bacon"></i>&nbsp;Entradas</button></a>
+                    <a href="#rangos"><button><i class="fa-solid fa-drumstick-bite"></i>&nbsp;Rangos</button></a>
+                    <a href="#drinks"><button><i class="fa-solid fa-martini-glass-citrus"></i>&nbsp;Drinks</button></a>
                 </div>
             </div>
         </div>
         <div class="content">
+
+            {{-- BURGS --}}
             <h1 id="burgs" class="lora-font" style="font-weight:bold;">Burgs</h1>
             <div class="posts lora-font">
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/hamburguer1.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$43,52</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52, '../images/hamburguer1.jpg')">Adicionar</button>
+                @foreach ($burgs as $item)
+                    <div class="row">
+                        <div class="post ">
+                            <img src="{{ asset('storage/' . $item->imagem) }}"
+                                alt="{{ asset('storage/' . $item->imagem) }}">
+                            <h3>{{ $item->nome }}</h3>
+                            <p>{{ $item->descricao }}</p>
+                            <p class="preco_produto" style="display:inline;">
+                                {{ 'R$' . number_format($item->preco, 2, ',', '.') }}</p>
+                            <button class="botao-add-carrinho"
+                                onclick="adicionarAoCarrinho('{{ $item->id }}', '{{ $item->nome }}', '{{ $item->preco }}', '{{ asset('storage/' . $item->imagem) }}')">Adicionar</button>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/hamburguer2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$43,52</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/hamburguer2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$43,52</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/hamburguer2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$43,52</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52)">Adicionar</button>
-                    </div>
-                </div>
+                @endforeach
             </div>
             <hr>
+
+
+
+            {{-- BURGS COM BATATA --}}
             <h1 id="burgsBatata" class="lora-font" style="font-weight:bold;">Burgs com Batata</h1>
             <div class="posts lora-font">
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/hamburguer2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo com 100g de Batata Frita</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$54,20</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52)">Adicionar</button>
+                @foreach ($burgsComBatata as $item)
+                    <div class="row">
+                        <div class="post ">
+                            <img src="{{ asset('storage/' . $item->imagem) }}"
+                                alt="{{ asset('storage/' . $item->imagem) }}">
+                            <h3>{{ $item->nome }}</h3>
+                            <p>{{ $item->descricao }}</p>
+                            <p class="preco_produto" style="display:inline;">
+                                {{ 'R$' . number_format($item->preco, 2, ',', '.') }}</p>
+                            <button class="botao-add-carrinho"
+                                onclick="adicionarAoCarrinho('{{ $item->nome }}', '{{ $item->preco }}', '{{ asset('storage/' . $item->imagem) }}')">Adicionar</button>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/hamburguer2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo com 100g de Batata Frita</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$54,20</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/hamburguer2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo com 100g de Batata Frita</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$54,20</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/hamburguer2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo com 100g de Batata Frita</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$54,20</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 43.52)">Adicionar</button>
-                    </div>
-                </div>
+                @endforeach
             </div>
             <hr>
+
+
+
+            {{-- ENTRADAS --}}
             <h1 id="entradas" class="lora-font" style="font-weight:bold;">Entradas</h1>
             <div class="posts lora-font">
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/batata1.jpg" alt="">
-                        <h3>Porção de Fritas</h3>
-                        <p>300g de batata frita, acompanhado com ketchup e maionese, temperada com alho.</p>
-                        <p class="preco_produto" style="display:inline;">R$21,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Porção de Fritas', 21.00)">Adicionar</button>
+                @foreach ($entradas as $item)
+                    <div class="row">
+                        <div class="post ">
+                            <img src="{{ asset('storage/' . $item->imagem) }}"
+                                alt="{{ asset('storage/' . $item->imagem) }}">
+                            <h3>{{ $item->nome }}</h3>
+                            <p>{{ $item->descricao }}</p>
+                            <p class="preco_produto" style="display:inline;">
+                                {{ 'R$' . number_format($item->preco, 2, ',', '.') }}</p>
+                            <button class="botao-add-carrinho"
+                                onclick="adicionarAoCarrinho('{{ $item->nome }}', '{{ $item->preco }}', '{{ asset('storage/' . $item->imagem) }}')">Adicionar</button>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata2.jpg" alt="">
-                        <h3>Fritas com Cheddar e Bacon</h3>
-                        <p>400g de batata frita, banhadas ao cheddar juntamente com pedaços de bacon e temperos.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00,'../images/batata2.jpg')">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata3.jpg" alt="">
-                        <h3>Fritas com Cheddar e Bacon</h3>
-                        <p>500g de batata frita, banhadas ao cheddar juntamente com pedaços de bacon e temperos.</p>
-                        <p class="preco_produto" style="display:inline;">R$40,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 40.00)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Hamburguer Pão e Ovo', 30.00)">Adicionar</button>
-                    </div>
-                </div>
+                @endforeach
             </div>
             <hr>
+
+
+            {{-- RANGOS --}}
             <h1 id="rangos" class="lora-font" style="font-weight:bold;">Rangos</h1>
             <div class="posts lora-font">
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/batata1.jpg" alt="">
-                        <h3>Porção de Fritas</h3>
-                        <p>300g de batata frita, acompanhado com ketchup e maionese, temperada com alho.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Porção de Fritas', 30.00)">Adicionar</button>
+                @foreach ($rangos as $item)
+                    <div class="row">
+                        <div class="post ">
+                            <img src="{{ asset('storage/' . $item->imagem) }}"
+                                alt="{{ asset('storage/' . $item->imagem) }}">
+                            <h3>{{ $item->nome }}</h3>
+                            <p>{{ $item->descricao }}</p>
+                            <p class="preco_produto" style="display:inline;">
+                                {{ 'R$' . number_format($item->preco, 2, ',', '.') }}</p>
+                            <button class="botao-add-carrinho"
+                                onclick="adicionarAoCarrinho('{{ $item->nome }}', '{{ $item->preco }}', '{{ asset('storage/' . $item->imagem) }}')">Adicionar</button>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata2.jpg" alt="">
-                        <h3>Fritas com Cheddar e Bacon</h3>
-                        <p>400g de batata frita, banhadas ao cheddar juntamente com pedaços de bacon e temperos.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata3.jpg" alt="">
-                        <h3>Fritas com Cheddar e Bacon</h3>
-                        <p>500g de batata frita, banhadas ao cheddar juntamente com pedaços de bacon e temperos.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00)">Adicionar</button>
-                    </div>
-                </div>
+                @endforeach
             </div>
             <hr>
+
+
+
+            {{-- DRINKS --}}
             <h1 id="drinks" class="lora-font" style="font-weight:bold;">Drinks</h1>
             <div class="posts lora-font">
-                <div class="row">
-                    <div class="post ">
-                        <img src="../images/batata1.jpg" alt="">
-                        <h3>Porção de Fritas</h3>
-                        <p>300g de batata frita, acompanhado com ketchup e maionese, temperada com alho.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00)">Adicionar</button>
+                @foreach ($drinks as $item)
+                    <div class="row">
+                        <div class="post ">
+                            <img src="{{ asset('storage/' . $item->imagem) }}"
+                                alt="{{ asset('storage/' . $item->imagem) }}">
+                            <h3>{{ $item->nome }}</h3>
+                            <p>{{ $item->descricao }}</p>
+                            <p class="preco_produto" style="display:inline;">
+                                {{ 'R$' . number_format($item->preco, 2, ',', '.') }}</p>
+                            <button class="botao-add-carrinho"
+                                onclick="adicionarAoCarrinho('{{ $item->nome }}', '{{ $item->preco }}', '{{ asset('storage/' . $item->imagem) }}')">Adicionar</button>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata2.jpg" alt="">
-                        <h3>Fritas com Cheddar e Bacon</h3>
-                        <p>400g de batata frita, banhadas ao cheddar juntamente com pedaços de bacon e temperos.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata3.jpg" alt="">
-                        <h3>Fritas com Cheddar e Bacon</h3>
-                        <p>500g de batata frita, banhadas ao cheddar juntamente com pedaços de bacon e temperos.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00)">Adicionar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="post">
-                        <img src="../images/batata2.jpg" alt="">
-                        <h3>Hamburguer Pão e Ovo</h3>
-                        <p>O hamburguer possui ovo, queijo, bacon, picles, molho especial, cebola roxa e 140g de carne
-                            bovina.</p>
-                        <p class="preco_produto" style="display:inline;">R$30,00</p>
-                        <button class="botao-add-carrinho" onclick="adicionarAoCarrinho('Fritas com Cheddar e Bacon', 30.00)">Adicionar</button>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
+
+
+
+
     </div>
 
     <button class="cart-btn" onclick="abrirCarrinho()">
@@ -274,7 +173,7 @@
 
         <div class="cart-footer">
             <p>Total: <span id="cartTotal">R$0,00</span></p>
-            <button class="btn btn-primary finalizar-compra-btn" onclick="finalizarCompra()">Finalizar Compra</button>
+            <button class="btn btn-primary finalizar-compra-btn" onclick="finalizarPedido()">Finalizar Pedido</button>
         </div>
     </div>
 
