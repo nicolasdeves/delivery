9a82b1565694427b1cc4ebd1cc476d125c38c64a
Nícolas Deves <nicolas.deves@universo.univates.br>
2024-11-01 13:36:43 -0300

    Feat: adicionar status pedido produto

--- app/Models/PedidoProduto.php
+++ app/Models/PedidoProduto.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PedidoProduto extends Model
 {
+    const STATUS_PEDIDO_ENVIADO = 0;
+    const STATUS_PEDIDO_PREPARANDO = 1;
+    const STATUS_PEDIDO_PRONTO = 2;
+    const STATUS_PEDIDO_ENTREGANDO = 3;
+    const STATUS_PEDIDO_ENTREGUE = 4;
+
+    const STATUS_PAGAMENTO_PENDENTE = 0;
+    const STATUS_PAGAMENTO_APROVADO = 1;
+
     protected $table = 'pedido_produto';
 
     protected $fillable = [
